dc58c9db4b8c58ee3c90d28bd8dbd4e3fb478ea0
Pedro Boas <pedrokvb@gmail.com>
2023-12-18 13:05:42 -0300

    remove container

--- src/app/pages/tabs/tab1/tab1.page.ts
+++ src/app/pages/tabs/tab1/tab1.page.ts
@@ -6,7 +6,6 @@ import {
   IonTitle,
   IonToolbar,
 } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../../../components/explore-container/explore-container.component';
 
 @Component({
   standalone: true,
@@ -15,46 +14,17 @@ import { ExploreContainerComponent } from '../../../components/explore-container
     IonToolbar,
     IonTitle,
     IonContent,
-    ExploreContainerComponent,
     RouterOutlet,
     RouterLink,
     RouterLinkActive,
   ],
   template: `
-    <ion-header [translucent]="true">
-      <ion-toolbar>
-        <ion-title> Tab 1 </ion-title>
-      </ion-toolbar>
-    </ion-header>
-
     <ion-content [fullscreen]="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-      <div
-        style="display: flex; flex-direction: column; gap:10px; padding:10px"
-      >
-        <a routerLink="/auth/login" routerLinkActive="active">/auth/login</a>
-        <a routerLink="/auth/sign-up" routerLinkActive="/auth/sign-up"
-          >/auth/sign-up</a
-        >
-      </div>
-
-      <div
-        style="display: flex; flex-direction: column; gap:10px; padding:10px"
-      >
-        <a routerLink="/tabs/tab2" routerLinkActive="active">/tabs/tab2</a>
-      </div>
-
-      <div
-        style="display: flex; flex-direction: column; gap:10px; padding:10px"
-      >
-        <a routerLink="/onboarding" routerLinkActive="active">/onboarding</a>
-      </div>
-
-      <app-explore-container name="Tab 1 page"></app-explore-container>
     </ion-content>
   `,
 })
--- src/app/pages/tabs/tab2/tab2.page.ts
+++ src/app/pages/tabs/tab2/tab2.page.ts
@@ -6,53 +6,18 @@ import {
   IonTitle,
   IonToolbar,
 } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../../../components/explore-container/explore-container.component';
 
 @Component({
   selector: 'app-tab2',
   standalone: true,
-  imports: [
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonContent,
-    ExploreContainerComponent,
-    RouterLink,
-  ],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, RouterLink],
   template: `
-    <ion-header [translucent]="true">
-      <ion-toolbar>
-        <ion-title> Tab 2 </ion-title>
-      </ion-toolbar>
-    </ion-header>
-
     <ion-content [fullscreen]="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Tab 2</ion-title>
         </ion-toolbar>
       </ion-header>
-      <div
-        style="display: flex; flex-direction: column; gap:10px; padding:10px"
-      >
-        <a routerLink="/auth/login" routerLinkActive="active">/auth/login</a>
-        <a routerLink="/auth/sign-up" routerLinkActive="/auth/sign-up"
-          >/auth/sign-up</a
-        >
-      </div>
-
-      <div
-        style="display: flex; flex-direction: column; gap:10px; padding:10px"
-      >
-        <a routerLink="/tabs/tab1" routerLinkActive="active">/tabs/tab1</a>
-      </div>
-
-      <div
-        style="display: flex; flex-direction: column; gap:10px; padding:10px"
-      >
-        <a routerLink="/onboarding" routerLinkActive="active">/onboarding</a>
-      </div>
-      <app-explore-container name="Tab 2 page"></app-explore-container>
     </ion-content>
   `,
 })
